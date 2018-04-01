89c7a576c753d5d8c90163d17054ebab1ea24f76
Andrey Frolov <andrunah@gmail.com>
2018-04-02 02:29:22 +0300

    Parser update

--- get_data.sh
+++ get_data.sh
@@ -8,6 +8,6 @@ PAGE="http://www.ganjawars.ru/object.php?id=69403&page=oncoming1&sid=116"
 PAGE_WARS="http://www.ganjawars.ru/wargroup.php?war=attacks"
 SITE="https://www.ganjawars.ru"; curl -A "$USERAGENT" -e "$SITE"/login.php "$SITE"/login.php > login
 LOGINKEY=`grep "loginkey " login | cut -d "'" -f 2`; LOGINKEYMD=`grep "loginkeymd " login | cut -d "'" -f 2`; sleep 1; curl -A "$USERAGENT" -d "resl=Null.&time=Null.&date=Null.&pwdmd5=Null.&pass1=&loginkey=$LOGINKEY&loginkeymd=$LOGINKEYMD&login=$LOGIN&pass=$PASSWD" "$SITE"/login.php -e "$SITE"/login.php -D headers_cookies
-curl -b headers_cookies "$PAGE" -A "$USERAGENT" -e "$PAGE" > port_116.html ; iconv -f cp1251 -t utf-8 port_116.html > port_list.html
+curl -b headers_cookies "$PAGE" -A "$USERAGENT" -e "$PAGE" > port_list.html
 # curl -b headers_cookies "$PAGE_WARS" -A "$USERAGENT" -e "$PAGE_WARS" > wars.html ; iconv -f cp1251 -t utf-8 wars.html > attack_list.html
 rm login headers_cookies port_116.html
--- parse_data.py
+++ parse_data.py
@@ -1,16 +1,32 @@
+import codecs
+
 from bs4 import BeautifulSoup as bs
 from html_table_parser import parser_functions as parse
+from tabulate import tabulate
 
 doc = ''
+resultset = [[]]
 
-with open('port_list.html', 'r') as fd:
+
+with codecs.open('port_list.html', 'r', encoding='cp1251', errors='ignore') as fd:
     for line in fd:
         line = line.lstrip()
+        line = line.replace("&nbsp;", " ")
         doc += line.replace("\n", " ")
 
 soup = bs(doc, "html.parser")
 test_table = soup.find('table', {'cellspacing': '1', 'cellpadding': '5', 'width': '100%'})
 twod_array = parse.make2d(test_table)
+twod_array[0] = ['Время', 'Формат', 'Участники', 'Контроль']
 
-# print 2D array
-print(twod_array)
+for battle in twod_array:
+    i = 0
+    del battle[3]
+    if battle[2] != 'Участники':
+        sind1, sind2 = str(battle[2]).split(' vs ', 1)
+        sind_1 = sind1.split(' ', 1)
+        sind_2 = sind2.split(' ', 1)
+        tlist = [battle[0], battle[1], sind_1[0].replace('#', ''), sind_1[1], sind_2[0].replace('#', ''), sind_2[1]]
+        resultset.append(tlist)
+        i += i
+print(tabulate(resultset, tablefmt='simple'))
